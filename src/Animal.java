46c65b9ff9dbf3d9ef9dbe08046f2c960c887926
Nikita Rochev <nikita.rochev@teamup-soft.ru>
2023-01-16 20:02:55 +0300

    Принципы ООП

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+
+    public void test() {
+        System.out.println("Я родитиельский");
+    }
+
+    public void test(String name){
+        System.out.println("Меня зовут " + name );
+    }
+}
